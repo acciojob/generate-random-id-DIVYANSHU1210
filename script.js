efc8c424d79864ecf8ba8eb79f3dfa82e85703b1
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2024-03-16 12:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function makeid(l) {
-  // write your code here
-}
+	const charStr = "abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	  let res = "";
+	// write your code here  
+	for(let i=0; i<l; i++){
+		const p = Math.floor((Math.random()*charStr.length));
+		res += charStr.charAt(p)
+	}
+	return res;
+} 
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
